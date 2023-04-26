2b20834fb24fce244f3d6503879ae9b640f134a1
JesusMapacho <128089204+JesusMapacho@users.noreply.github.com>
2023-04-26 13:28:46 -0600

    añadido

---
+++ Tabs/SubMediaDUR/M_MaIn.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>MediaDUR</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="keywords" content="HTML5 Template">
+        <meta name="description" content="Mist — Multi-Purpose HTML Template">
+        <meta name="author" content="zozothemes.com">
+        <meta name="viewport" conte-nt="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../../rs-plugin/images/logoicon.png">
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Arimo:300,400,700,400italic,700italic'>
+        <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='../../css/font-awesome.min.css' rel='stylesheet' type='text/css'/>
+        <link href="../../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../css/hover-dropdown-menu.css" rel="stylesheet">
+        <link href="../../css/icons.css" rel="stylesheet">
+        <link href="../../css/revolution-slider.css" rel="stylesheet">
+        <link href="../../rs-plugin/css/settings.css" rel="stylesheet">
+        <link href="../../css/animate.min.css" rel="stylesheet">
+        <link href="../../css/owl/owl.carousel.css" rel="stylesheet" >
+        <link href="../../css/owl/owl.theme.css" rel="stylesheet" >
+        <link href="../../css/owl/owl.transitions.css" rel="stylesheet" >
+        <link href="../../css/prettyPhoto.css" rel="stylesheet">
+        <link href="../../css/style.css" rel="stylesheet">
+		<link href="../../css/responsive.css" rel="stylesheet">
+        <link href="../../css/color.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="page">		
+
+			<!-- Page Loader -->
+			<div id="pageloader">
+				<div class="loader-item fa fa-spin text-color"></div>
+			</div>
+
+            <!-- Header -->
+            <?php
+            require_once '../../components/nav.php';
+            $nav = new Nav();
+session_start();
+
+            if (!isset($_SESSION['login'])) {
+                $_SESSION['login'] = false ;
+                header("location:". $nav->host ."/Tabs/SubMediaDUR/M_Principal.php");
+            }
+            $nav->setTitle( 'Maltrato Infantil' );
+            $nav->setHaveLogout( true );
+            $nav->setHaveDropdown( true );
+            $nav->setHaveLogin(false);
+            $nav->render();
+            ?>
+            <section class="page-section page-section-video">
+                <div class="futuro-video"><p>(Video de presentación del protocolo)</p></div>
+            </section>
+
+            <!--Tabla de contenido-->
+            <section class="page-section">
+                <h4 style="  text-align: center; color: #2e9797; font-weight: bolder;">3. Protocolo de detección, prevención y actuación en casos de Maltrato Infantil.</h4>
+                <div class="container boxed-block" style="border-radius: 15px; text-align: center;">
+                    <br>
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1qkBHIH_5uYRLKP7OOPXouu6t4VBEVWys/view">3.0. Protocolo de detección, prevención y actuación en casos de Maltrato Infantil. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1bu1d9xRoi6oVqsJv83CFbayglsGf0dbM/view">3.1. Responsabilidades de Madres, Padres y Tutores Legales. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/19cKbYHOEJrelM6GK4ICTsioEGp1oyW15/view">3.2. Responsabilidades de los Docentes. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1eiWARlTl8j6JKCjhN-hia8IqqEaKWrM3/view">3.3. Responsabilidades de Personal Administrativo, De Apoyo. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1_qZ7bVqINQNBBXhDIMr77bj8gYVlBCO_/view">3.4. Responsabilidades del Personal de Servicios Educativos Complementarios. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1qJrDIlYPlX3GcJzFCFNnNnrSTRo7_X5n/view">3.5. Responsabilidades de Directivos Escolares. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1herr_r6PNWi7vM4Vko_8gGX3MbKR035i/view">3.6. Responsabilidades de los Jefes de Sector y Supervisores(as). <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1AJAiXxK5Pqqwnjy1vH4Lk3TWcj7ONB05/view">3.7. Guía de observación para identificar indicadores de riesgo y detección en casos de maltrato infantil. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/11KbcR8oiA1eGO0utQBvtnPfqSUXsritM/view">3.8. Protocolo en situaciones de maltrato infantil. <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                    <hr />
+                    <div class="row">
+                        <div class="col-md-12" ><a href="https://drive.google.com/file/d/1aejZTRWVO1S3joFwtNt2MjDaCbUfquxt/view">3.9. Diagrama de Flujo del Protocolo de Maltrato Infantil (Infografía). <img  src="../../Resources/Usr/ad.png" alt="" width="100px" height="60px"></a></div>
+                    </div>
+                </div>
+            </section>
+            
+            <!--Footer -->
+            <footer id="footer">
+                <div class="copyright">
+                    <div class="container">
+                        <div class="row">
+                        <div class="col-xs-10 col-sm-6 col-md-6"> &copy; 2023. Apoyo educativo.                            <br />
+                            <br />
+                            <a href="#">Terminos y Condiciones</a> / 
+                            <a href="#">Politica y Privacidad</a></div>
+                            <div class="col-xs-2  col-sm-6 col-md-6 text-right page-scroll gray-bg icons-circle i-3x">
+                                <a href="#page">
+                                    <i class="glyphicon glyphicon-arrow-up"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            
+        </div>
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="../../js/jquery.min.js"></script>
+        <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../../js/hover-dropdown-menu.js"></script>
+        <script type="text/javascript" src="../../js/jquery.hover-dropdown-menu-addon.js"></script>	
+        <script type="text/javascript" src="../../js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="../../js/jquery.sticky.js"></script>
+        <script type="text/javascript" src="../../js/bootstrapValidator.min.js"></script>	
+        <script type="text/javascript" src="../../rs-plugin/js/jquery.themepunch.tools.min.js"></script>   
+        <script type="text/javascript" src="../../rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="../../js/revolution-custom.js"></script>
+        <script type="text/javascript" src="../../js/jquery.mixitup.min.js"></script>
+        <script type="text/javascript" src="../../js/jquery.appear.js"></script>
+        <script type="text/javascript" src="../../js/effect.js"></script>
+        <script type="text/javascript"  src="../../js/owl.carousel.min.js"></script>
+        <script type="text/javascript"  src="../../js/jquery.prettyPhoto.js"></script>
+        <script type="text/javascript"  src="../../js/jquery.parallax-1.1.3.js"></script>
+        <script type="text/javascript"  src="../../js/jquery.countTo.js"></script>
+        <script type="text/javascript" src="../../js/tweet/carousel.js"></script>
+        <script type="text/javascript" src="../../js/tweet/scripts.js"></script>
+        <script type="text/javascript" src="../../js/tweet/tweetie.min.js"></script>
+        <script type="text/javascript" src="../../js/jquery.mb.YTPlayer.js"></script>
+        <script type="text/javascript" src="../../js/custom.js"></script>
+    </body>
+</html>
