ef77344083be058f957a1414ace12fae947c22e0
Gustavo Mayorga <redvioletoficial@gmail.com>
2023-04-26 23:21:05 -0500

    guardando base para diseño de acerca de

--- Tabs/objetivos.php
+++ Tabs/objetivos.php
@@ -1,140 +1,145 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Acerca de</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="keywords" content="HTML5 Template">
-        <meta name="description" content="Mist — Multi-Purpose HTML Template">
-        <meta name="author" content="zozothemes.com">
-        <meta name="viewport" conte-nt="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="../rs-plugin/images/logoicon.png">
-        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Arimo:300,400,700,400italic,700italic'>
-        <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
-        <link href='../css/font-awesome.min.css' rel='stylesheet' type='text/css'/>
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-        <link href="../css/hover-dropdown-menu.css" rel="stylesheet">
-        <link href="../css/icons.css" rel="stylesheet">
-        <link href="../css/revolution-slider.css" rel="stylesheet">
-        <link href="../rs-plugin/css/settings.css" rel="stylesheet">
-        <link href="../css/animate.min.css" rel="stylesheet">
-        <link href="../css/owl/owl.carousel.css" rel="stylesheet" >
-        <link href="../css/owl/owl.theme.css" rel="stylesheet" >
-        <link href="../css/owl/owl.transitions.css" rel="stylesheet" >
-        <link href="../css/prettyPhoto.css" rel="stylesheet">
-        <link href="../css/style.css" rel="stylesheet">
-		<link href="../css/responsive.css" rel="stylesheet">
-        <link href="../css/color.css" rel="stylesheet">
-    </head>
-    <body>
-        <div id="page">		
 
-			<!-- Page Loader -->
-			<div id="pageloader">
-				<div class="loader-item fa fa-spin text-color"></div>
-			</div>
+<head>
+    <meta charset="utf-8">
+    <title>Acerca de</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="Mist — Multi-Purpose HTML Template">
+    <meta name="author" content="zozothemes.com">
+    <meta name="viewport" conte-nt="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../rs-plugin/images/logoicon.png">
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Arimo:300,400,700,400italic,700italic'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='../css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/hover-dropdown-menu.css" rel="stylesheet">
+    <link href="../css/icons.css" rel="stylesheet">
+    <link href="../css/revolution-slider.css" rel="stylesheet">
+    <link href="../rs-plugin/css/settings.css" rel="stylesheet">
+    <link href="../css/animate.min.css" rel="stylesheet">
+    <link href="../css/owl/owl.carousel.css" rel="stylesheet">
+    <link href="../css/owl/owl.theme.css" rel="stylesheet">
+    <link href="../css/owl/owl.transitions.css" rel="stylesheet">
+    <link href="../css/prettyPhoto.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/responsive.css" rel="stylesheet">
+    <link href="../css/color.css" rel="stylesheet">
+</head>
 
-            <!-- Header -->
-            <?php
-            require '../components/nav.php';
-            $nav = new Nav();
-            $nav->setTitle( 'Misión, Visión y Servicios' );
-            session_start();
-            if (!isset($_SESSION['login'])) {
-                $_SESSION['login'] = false ;
-            }
-            $nav->setHaveLogin(!$_SESSION['login']);
-            $nav->setHaveLogout( $_SESSION['login'] );
-            $nav->setHaveDropdown( $_SESSION['login'] );
-            $nav->render();
-            ?>
+<body>
+    <div id="page">
 
-            <!-- RELLENO-->
-            <section class="page-section">
-                <div class="container">
-                    <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Mision</h4>
-                    <div class="row">
-                        <div class="col-md-12" style="font-style: italic">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet illo consectetur quisquam velit blanditiis..."</div>
-                    </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate.</div>
-                    </div>
-                    <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Vision</h4>
-                    <div class="row">
-                        <div class="col-md-12" style="font-style: italic">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet illo consectetur quisquam velit blanditiis..."</div>
-                    </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate.</div>
-                    </div>
-                    <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Servicios</h4>
-                    <div class="row">
-                        <div class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate.</div>
-                    </div>
-                    <br>
-                    <ol>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, error.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, error.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, error.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, error.</li>
-                      </ol>
-                    <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Departamento de la subsecretaria</h4>
-                    <div class="row">
-                        <div class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima voluptatem assumenda voluptas quidem sit maiores odio velit voluptate.</div>
+        <!-- Page Loader -->
+        <div id="pageloader">
+            <div class="loader-item fa fa-spin text-color"></div>
+        </div>
+
+        <!-- Header -->
+        <?php
+        require '../components/nav.php';
+        $nav = new Nav();
+        $nav->setTitle('SEED');
+        session_start();
+        if (!isset($_SESSION['login'])) {
+            $_SESSION['login'] = false;
+        }
+        $nav->setHaveLogin(!$_SESSION['login']);
+        $nav->setHaveLogout($_SESSION['login']);
+        $nav->setHaveDropdown($_SESSION['login']);
+        $nav->render();
+        ?>
+
+        <!-- RELLENO-->
+        <section class="page-section">
+            <div class="container">
+                <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Mision</h4>
+
+                <div class="row">
+                    <div class="col-md-12"> Ofrecer una educación de calidad, integral, participativa,
+                        innovadora y promotora del aprendizaje que nos permita formar personas comprometidas con
+                        el desarrollo sostenible, capaces de elevar su nivel de vida, el desarrollo personal y la
+                        formación de capital humano,
+                        que contribuya al impulso del desarrollo económico y social del Estado.</div>
+                </div>
+                <br>
+                <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Vision</h4>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        Ser un sistema educativo sólido, con una educación para todos, con sentido humano y social,
+                        que orienta su actuación bajo el impulso de la equidad, la rendición de cuentas,
+                        la transparencia y ofrece servicios educativos, administrativos con eficiencia, calidad y
+                        pertinencia.
                     </div>
                 </div>
-            </section>
+                <br>
+                <h4 style="color: rgb(42, 42, 46); font-weight: bold;">Objetivo</h4>
+
+                <div class="row">
+                    <div class="col-md-12">
+                     <p>La Secretaría de Educación, instancia rectora de la política educativa, posee una actividad funcional que considera la planeación, operación y control de actividades diversas, cuya conducción está a cargo de un equipo humano multidisciplinario, que labora en espacios físicos variados, integrando un conjunto de fuerzas en movimiento, ha operado con elevados niveles de efectividad, para ofrecer el servicio educativo con calidez humana.</p>
+                     <br>
+                     <p>Tiene establecidos tanto planes estratégicos planes operativos, metas generales, visión y misión. Los proyectos estratégicos, diseñados para alcanzar los objetivos y las metas, muestran cómo se pueden aplicar en el quehacer cotidiano, están ligados a la definición de la misión de la Dependencia, el objetivo general que justifica su existencia.</p>
+                     <br>
+                     <p>Indudablemente, las metas son elementos fundamentales en la operatividad de la Secretaría, que se perfilan a un futuro deseable, proporcionan sentido de dirección, concentran esfuerzos, guían planes y decisiones; sirven para evaluar los avances. El grado en que se alcancen las metas, depende del desempeño de los directivos, así como de la eficacia de quienes coordinan las acciones de los niveles operativos.</p>
+                    </div>
+                </div>
+                <br>
+        </section>
 
 
-            <div class="page-header" style="background-color: #808080; ">
-                <div class="container" style="display: block; text-align: center; width: auto;">
-                    <!-- <img src="../Resources/Logos/CIIDE_BackGray.png" width="350" style="margin-right: 25px;"> -->
-                    <img src="../rs-plugin/images/SEED.svg" width="250" style="margin-left: 25px;">
+        <div class="page-header" style="background-color: #808080; ">
+            <div class="container" style="display: block; text-align: center; width: auto;">
+                <!-- <img src="../Resources/Logos/CIIDE_BackGray.png" width="350" style="margin-right: 25px;"> -->
+                <img src="../rs-plugin/images/SEED.svg" width="250" style="margin-left: 25px;">
+            </div>
+        </div>
+
+        <!--Footer -->
+        <footer id="footer">
+            <div class="copyright">
+                <div class="container">
+                    <div class="row">
+                        <!-- <div class="col-xs-10 col-sm-6 col-md-6"> &copy; 2021 <a href="http://deciide.com.mx">deciide.com.mx</a>. Apoyo educativo. -->
+                        <br />
+                        <a href="#">Terminos y Condiciones</a> /
+                        <a href="#">Politica y Privacidad</a>
+                    </div>
+                    <div class="col-xs-2  col-sm-6 col-md-6 text-right page-scroll gray-bg icons-circle i-3x">
+                        <a href="#page">
+                            <i class="glyphicon glyphicon-arrow-up"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
+    </div>
+    </footer>
 
-            <!--Footer -->
-            <footer id="footer">
-                <div class="copyright">
-                    <div class="container">
-                        <div class="row">
-                            <!-- <div class="col-xs-10 col-sm-6 col-md-6"> &copy; 2021 <a href="http://deciide.com.mx">deciide.com.mx</a>. Apoyo educativo. -->
-                            <br />
-                            <a href="#">Terminos y Condiciones</a> / 
-                            <a href="#">Politica y Privacidad</a></div>
-                            <div class="col-xs-2  col-sm-6 col-md-6 text-right page-scroll gray-bg icons-circle i-3x">
-                                <a href="#page">
-                                    <i class="glyphicon glyphicon-arrow-up"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+    <!-- Scripts -->
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/hover-dropdown-menu.js"></script>
+    <script type="text/javascript" src="../js/jquery.hover-dropdown-menu-addon.js"></script>
+    <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="../js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="../js/bootstrapValidator.min.js"></script>
+    <script type="text/javascript" src="../rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="../rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="../js/revolution-custom.js"></script>
+    <script type="text/javascript" src="../js/jquery.mixitup.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../js/effect.js"></script>
+    <script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="../js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="../js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="../js/tweet/carousel.js"></script>
+    <script type="text/javascript" src="../js/tweet/scripts.js"></script>
+    <script type="text/javascript" src="../js/tweet/tweetie.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.mb.YTPlayer.js"></script>
+    <script type="text/javascript" src="../js/custom.js"></script>
+</body>
 
-            <!-- Scripts -->
-            <script type="text/javascript" src="../js/jquery.min.js"></script>
-            <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-            <script type="text/javascript" src="../js/hover-dropdown-menu.js"></script>
-            <script type="text/javascript" src="../js/jquery.hover-dropdown-menu-addon.js"></script>	
-            <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
-            <script type="text/javascript" src="../js/jquery.sticky.js"></script>
-            <script type="text/javascript" src="../js/bootstrapValidator.min.js"></script>	
-            <script type="text/javascript" src="../rs-plugin/js/jquery.themepunch.tools.min.js"></script>   
-            <script type="text/javascript" src="../rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-            <script type="text/javascript" src="../js/revolution-custom.js"></script>
-            <script type="text/javascript" src="../js/jquery.mixitup.min.js"></script>
-            <script type="text/javascript" src="../js/jquery.appear.js"></script>
-            <script type="text/javascript" src="../js/effect.js"></script>
-            <script type="text/javascript"  src="../js/owl.carousel.min.js"></script>
-            <script type="text/javascript"  src="../js/jquery.prettyPhoto.js"></script>
-            <script type="text/javascript"  src="../js/jquery.parallax-1.1.3.js"></script>
-            <script type="text/javascript"  src="../js/jquery.countTo.js"></script>
-            <script type="text/javascript" src="../js/tweet/carousel.js"></script>
-            <script type="text/javascript" src="../js/tweet/scripts.js"></script>
-            <script type="text/javascript" src="../js/tweet/tweetie.min.js"></script>
-            <script type="text/javascript" src="../js/jquery.mb.YTPlayer.js"></script>
-            <script type="text/javascript" src="../js/custom.js"></script>
-        </body>
-    </html>
+</html>
